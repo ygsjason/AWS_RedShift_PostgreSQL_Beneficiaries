7e61d6891e762f65ca4ab0da39cc0153a2c58bef
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-03-07 10:25:16 -0600

    Create Pull_ACO_Attribution.sql

---
+++ Pull_ACO_Attribution.sql
@@ -0,0 +1,29 @@
+WITH attrib AS (select * from(
+select empi,
+		last_value(orgn ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS orgn,
+		last_value(orgtin ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS orgtin,
+		last_value(pcpnpi ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS pcpnpi,
+		last_value(fn ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS fn,
+		last_value(ln ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt  asc rows UNBOUNDED PRECEDING) AS ln,
+		last_value(mn ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS mn,
+		last_value(gn ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS gn,
+		last_value(pcpn ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS pcpn,
+		last_value(sln ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS sln,
+		last_value(prnm ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS prnm,
+		last_value(plnm ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS plnm,
+		last_value(rn ignore nulls) OVER (PARTITION BY empi ORDER BY atrdt asc rows UNBOUNDED PRECEDING) AS rn,
+		ROW_NUMBER() over (partition by empi order by atrdt::date desc,ingdt::date desc) as rowno 
+from l2.pd_attribution
+where lower(atrl)='aco'
+--and empi = 'P1081671'
+AND plid IN ('1','2','3','10','22','29')
+and atrdt >= '2021-01-01'
+)
+where rowno=1),
+id AS (SELECT DISTINCT empi AS empi_id,id 
+from l2.pd_attribution pa 
+where atrdt >= '2021-01-01'
+and lower(atrl) = 'payer'
+and plid IN ('1','2','3','10','22','29'))
+SELECT * FROM attrib 
+RIGHT OUTER JOIN id on empi = id.empi_id
